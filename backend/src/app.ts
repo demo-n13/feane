c03da65ffce5cff58ccbd6ed885efa707647df11
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-09-27 17:36:35 +0500

    add config module and sequelize ORM

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig, dbConfig } from '@config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig],
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        dialect: 'postgres',
+        host: config.get('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get('database.user'),
+        password: config.get('database.password'),
+        database: config.get('database.dbName'),
+        sync: { alter: true, force: false },
+        models: [],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 import { AppModule } from './app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  // SET GLOBAL PREFIX TO /api/v1
+  app.setGlobalPrefix('/api/v1');
+
+  // USE MORGAN IN DEVELOPMENT MODE
+  if (process.env?.NODE_ENV?.trim() == 'development') {
+    app.use(morgan('tiny'));
+  }
+
+  await app.listen(configService.get<number>('appConfig.port'), () => {
+    console.log(`Listening on ${configService.get<number>('appConfig.port')}`);
+  });
 }
 bootstrap();
