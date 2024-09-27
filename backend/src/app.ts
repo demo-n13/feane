4079f6c3b4743111f0048c0471b04bf5b9020b2e
Abduqodir65 <abduqodirgithub@gmail.com>
2024-09-27 17:19:16 +0500

    first commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { appConfig, dbConfig } from './config';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig]
+    })
+  ],
 })
 export class AppModule {}
