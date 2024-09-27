2615768eff8b50f481020d087f7b6626078607fa
Abduqodir65 <abduqodirgithub@gmail.com>
2024-09-27 17:38:16 +0500

    sequelize added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { appConfig, dbConfig } from './config';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { config } from 'process';
 
 
 @Module({
@@ -8,6 +10,21 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig, dbConfig]
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        dialect: "postgres",
+        host: config.get('database.host'),
+        port: +config.get('database.port'),
+        username: config.get('database.user'),
+        password: config.get('database.password'),
+        database: config.get('database.dbName'),
+        models: [],
+        sync: {alter:true, force:true},
+      }),
+      inject: [ConfigService]
+
     })
   ],
 })
