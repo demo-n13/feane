f5009dfac2aa22a6cefaec185f0bdac86a4bad51
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-10-09 14:58:16 +0500

    add /me endpoint

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -4,10 +4,12 @@ import { UploadService } from '../upload';
 import { UserController } from './user.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models';
+import { MeController } from './me.controller';
+import { MeService } from './me.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([User])],
-  providers: [UserService, UploadService],
-  controllers: [UserController],
+  providers: [UserService, UploadService, MeService],
+  controllers: [UserController, MeController],
 })
 export class UserModule {}
