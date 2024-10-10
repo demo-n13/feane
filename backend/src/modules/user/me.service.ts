fd3d90aeae6b4a12b8cdbc24ee54a7185134a43e
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-10-10 16:50:06 +0500

    add foods while getting /me

--- backend/src/modules/user/me.service.ts
+++ backend/src/modules/user/me.service.ts
@@ -3,12 +3,13 @@ import { InjectModel } from "@nestjs/sequelize";
 import { User } from "./models";
 import { Order, OrderItem } from "../order";
 import { Review } from "../review";
+import { Food } from "../food";
 
 @Injectable()
 export class MeService {
     constructor(@InjectModel(User) private userModel: typeof User) {}
 
     async getMe(userId: number) : Promise<User> {
-        return await this.userModel.findByPk(userId, {include: [{model: Order, include: [OrderItem]}, Review]})
+        return await this.userModel.findByPk(userId, {include: [{model: Order, include: [{model: OrderItem, include: [Food]}]}, Review]})
     }     
 }
